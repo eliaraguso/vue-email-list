3b8cb5f5fdc7a18bc11427d228d755955ce7cc31
eliaraguso <elia.raguso@gmail.com>
2021-09-27 17:03:42 +0200

    fixato main.js per far comparire i 10 indirizzi solo quando sono stati tutti completamente generati

--- Js/main.js
+++ Js/main.js
@@ -15,11 +15,14 @@ const app = new Vue({
 
     },
     mounted() {
+        emailsTemporary = [];
         for (let i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((response) => {
-                    this.emails.push(response.data.response)
+                    emailsTemporary.push(response.data.response);
+                    console.log(emailsTemporary);
                 });
-        }   
+        }
+        this.emails = emailsTemporary;
     }
 });
