cda01e490fd650a7aed9160b416864c5f0c52ae1
eliaraguso <elia.raguso@gmail.com>
2021-09-27 16:42:35 +0200

    inizio parte Js

--- Js/main.js
+++ Js/main.js
@@ -1,9 +1,29 @@
+// Descrizione:
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+
 const app = new Vue({
     el: '#root',
     data: {
-
+        emails: []
     },
     methods: {
-        
+
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            const element = this.emails[i];
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    this.emails.push(response.data.response)
+                    console.log(this.emails[i])
+                });
+            
+        }
+            
     }
 });
